d2604587939c1bf804b0c409e43f27a20d38b937
Travis Prosser <travisprosser@gmail.com>
2021-02-01 12:05:34 -0400

    get prices from product page

--- anbl_scraper/utils/scrape_utils.py
+++ anbl_scraper/utils/scrape_utils.py
@@ -3,4 +3,16 @@ def get_product_attrs(soup):
         a.text.strip(" :"): a.next_sibling.next_sibling.text.strip()
         for a in soup.find_all(class_="attribute-title")
     }
+
+    price_soup = soup.find(class_="price-group")
+    if price_soup.small:
+        reg_price = price_soup.small.text
+        sale_price = price_soup.strong.text
+    else:
+        reg_price = price_soup.strong.text
+        sale_price = None
+    
+    attrs_raw['reg_price'] = reg_price
+    attrs_raw['sale_price'] = sale_price
+
     return attrs_raw
