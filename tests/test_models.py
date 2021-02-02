444325fe3c0d15f6af2614aaeb45d035b734d178
Travis Prosser <travisprosser@gmail.com>
2021-02-01 23:57:56 -0400

    black formatting

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,11 @@
 from anbl_scraper.models import Product
 
+
 def test_product_from_dict(sample_product_list, sample_product):
-    prod = Product.from_dict(sample_product_list[0]) 
-    
+    prod = Product.from_dict(sample_product_list[0])
+
     assert vars(prod) == vars(sample_product)
+
 
 def test_product_to_dict(sample_product):
     d = sample_product.to_dict()
