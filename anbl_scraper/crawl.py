18e6605c3d762a3a461a06aa9f68a7d39e4ed1a4
Travis Prosser <travisprosser@gmail.com>
2021-02-03 02:12:56 -0400

    add argparse

--- anbl_scraper/crawl.py
+++ anbl_scraper/crawl.py
@@ -5,35 +5,87 @@ from anbl_scraper.utils.ajax_utils import (
     get_number_of_products,
     page_fetcher_factory,
 )
-
 from concurrent.futures import ThreadPoolExecutor
+import argparse
+import os
 
 
-def fetch_products_threaded(category, page_size, max_workers=20):
+def fetch_products_threaded(category, page_size, max_workers=20, dry_run=False):
     n_prods = get_number_of_products(category)
     n_pages = get_number_of_pages(n_prods, page_size)
-    fetcher = page_fetcher_factory(category, page_size)
+    fetch_func = page_fetcher_factory(category, page_size)
+
+    page_range = [1] if dry_run else range(1, n_pages + 1)
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        res = executor.map(fetcher, range(1, n_pages + 1))
+        res = executor.map(fetch_func, page_range)
 
     return [p for r in res for p in r]
 
 
-def main():
-    output_path = "~/Desktop/test.csv"
-    page_size = 20
+def crawl(output_path="~/Downloads", page_size=20, categories=None, dry_run=False):
+    if dry_run:
+        categories = ["beer"]
+    elif categories is None:
+        categories = CATEGORY_IDS.keys()
+    elif not set(categories).issubset(set(CATEGORY_IDS.keys())):
+        raise KeyError(f"Unknown keys: {set(categories) - set(CATEGORY_IDS.keys())}")
+
     products = []
     print(f"Fetching {page_size} products at a time per thread...")
-    for product_type in CATEGORY_IDS.keys():
-        product_list = fetch_products_threaded(product_type, page_size, max_workers=100)
+    for product_type in categories:
+        product_list = fetch_products_threaded(
+            product_type, page_size, max_workers=100, dry_run=dry_run
+        )
         print(f"Number of {product_type} products: {len(product_list)}")
         products.extend(product_list)
 
     if len(products):
-        write_product_link_csv(output_path, products)
+        if dry_run:
+            print("Products:\n" + "-" * 9)
+            for p in products:
+                print(p)
+        else:
+            write_product_link_csv(output_path, products)
     else:
         print("Can't save to file as no products were returned")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--output-path",
+        default="~/Downloads",
+        help="Path to output file or directory.",
+    )
+    parser.add_argument(
+        "-p",
+        "--page-size",
+        default=20,
+        help="Number of product results to fetch per request.",
+    )
+    parser.add_argument(
+        "-c",
+        "--categories",
+        nargs="+",
+        default=None,
+        help="Space-separated list of categories of product results to crawl.",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Scrape a single page of results and don't save to CSV.",
+    )
+    args = parser.parse_args()
+
+    print(f"Running with args: {vars(args)}")
+    crawl(
+        output_path=args.output_path,
+        page_size=args.page_size,
+        categories=args.categories,
+        dry_run=args.dry_run,
+    )
 
 
 if __name__ == "__main__":
