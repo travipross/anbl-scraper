b1eedecb94fc1c1728df9f9e1e24ff0e9241dcbe
Travis Prosser <travisprosser@gmail.com>
2021-02-04 01:27:42 -0400

    Improve scrape utils and add tests

--- anbl_scraper/utils/scrape_utils.py
+++ anbl_scraper/utils/scrape_utils.py
@@ -1,5 +1,9 @@
 def parse_price(raw_price):
-    return float(raw_price.lstrip("$").strip()) if raw_price is not None else None
+    return (
+        float(raw_price.lstrip("$").strip().replace(",", ""))
+        if raw_price is not None
+        else None
+    )
 
 
 def parse_abv(raw_abv):
@@ -20,21 +24,24 @@ def get_product_attrs(soup):
         a.text.strip(" :"): a.next_sibling.next_sibling.text.strip()
         for a in soup.find_all(class_="attribute-title")
     }
-    attrs_raw["abv_prct"] = parse_abv(attrs_raw.pop("Alcohol content"))
+    attrs_raw["abv_prct"] = parse_abv(attrs_raw.pop("Alcohol content", None))
     attrs_raw["quantity_per_container"] = parse_int(
-        attrs_raw.pop("Quantity per container")
+        attrs_raw.pop("Quantity per container", None)
     )
-    attrs_raw["container_size_ml"] = parse_vol(attrs_raw.pop("Container Size"))
-    attrs_raw["country_of_origin"] = attrs_raw.pop("Country of origin")
+    attrs_raw["container_size_ml"] = parse_vol(attrs_raw.pop("Container Size", None))
+    attrs_raw["country_of_origin"] = attrs_raw.pop("Country of origin", None)
 
     # Parse from price box
     price_soup = soup.find(class_="price-group")
-    if price_soup.small:
-        price_reg = price_soup.small.text
-        price_sale = price_soup.strong.text
+    if price_soup:
+        if price_soup.small:
+            price_reg = price_soup.small.text
+            price_sale = price_soup.strong.text
+        else:
+            price_reg = price_soup.strong.text
+            price_sale = None
     else:
-        price_reg = price_soup.strong.text
-        price_sale = None
+        price_reg = price_sale = None
 
     attrs_raw["price_reg"] = parse_price(price_reg)
     attrs_raw["price_sale"] = parse_price(price_sale)
--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -1,4 +1,11 @@
-from anbl_scraper.utils.scrape_utils import get_product_attrs
+from anbl_scraper.utils.scrape_utils import (
+    get_product_attrs,
+    parse_abv,
+    parse_int,
+    parse_price,
+    parse_vol,
+)
+import pytest
 
 
 def test_attrs_extract(sample_soup):
@@ -9,3 +16,31 @@ def test_attrs_extract(sample_soup):
     assert attrs["container_size_ml"] == 341
     assert attrs["country_of_origin"] == "Canada"
     assert attrs["price_reg"] == 25.49
+
+
+@pytest.mark.parametrize(
+    "in_val,out_val", [("1.23", 1.23), ("5 %", 5.0), ("  6.9 %", 6.9), (None, None)]
+)
+def test_parse_abv(in_val, out_val):
+    assert out_val == parse_abv(in_val)
+
+
+@pytest.mark.parametrize("in_val,out_val", [("1", 1), ("2", 2), (None, None)])
+def test_parse_int(in_val, out_val):
+    assert out_val == parse_int(in_val)
+
+
+@pytest.mark.parametrize(
+    "in_val,out_val",
+    [("$1234", 1234.0), ("$ 49.99", 49.99), ("$ 29,999", 29999.0), (None, None)],
+)
+def test_parse_price(in_val, out_val):
+    assert out_val == parse_price(in_val)
+
+
+@pytest.mark.parametrize(
+    "in_val,out_val",
+    [("355 mL", 355), ("1140", 1140), (None, None)],
+)
+def test_parse_vol(in_val, out_val):
+    assert out_val == parse_vol(in_val)
